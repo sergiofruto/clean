d98dfebed1205215e8485d11ec58c759f460ea08
sergiofruto <fruto.sergio@gmail.com>
2014-08-08 00:31:11 -0300

    Layout changes
    More changes to they layout

--- layout/columns3.php
+++ layout/columns3.php
@@ -64,17 +64,18 @@ echo $OUTPUT->doctype() ?>
     </header>
 
     <div id="page-content" class="row-fluid">
-        <div id="<?php echo $regionbsid ?>" class="span9">
-            <div class="row-fluid">
-                <section id="region-main" class="span8 pull-right">
-                    <?php
-                    echo $OUTPUT->course_content_header();
-                    echo $OUTPUT->main_content();
-                    echo $OUTPUT->course_content_footer();
-                    ?>
-                </section>
-                <?php echo $OUTPUT->blocks('side-pre', 'span4 desktop-first-column'); ?>
-            </div>
+        <?php echo $OUTPUT->blocks('side-pre', 'span3 desktop-first-column'); ?>
+            <div id="<?php echo $regionbsid ?>" class="span6">
+                <div class="row-fluid">
+                    <div class="span12 social-bar"></div>
+                    <section id="region-main" class="span8 pull-right">
+                        <?php
+                        echo $OUTPUT->course_content_header();
+                        echo $OUTPUT->main_content();
+                        echo $OUTPUT->course_content_footer();
+                        ?>
+                    </section>
+                </div>
         </div>
         <?php echo $OUTPUT->blocks('side-post', 'span3'); ?>
     </div>
